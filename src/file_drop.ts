ca485559e94081b0a2919777afa38774a4039625
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-06 14:11:00 +0100

    feat: finish file drop

--- src/file_drop.ts
+++ src/file_drop.ts
@@ -1,7 +1,7 @@
-// TODO
-// RLAPI bool IsFileDropped(void);                                   // Check if a file has been dropped into window
-// RLAPI FilePathList LoadDroppedFiles(void);                        // Load dropped filepaths
-// RLAPI void UnloadDroppedFiles(FilePathList files);                // Unload dropped filepaths
+/**
+ * File drop functions
+ * @module
+ */
 import { lib } from "../bindings/bindings.ts";
 
 export class FileDrop {
@@ -14,12 +14,12 @@ export class FileDrop {
     const view = new DataView(result.buffer);
     const length = view.getUint32(4, true);
     const pointer = Deno.UnsafePointer.create(view.getBigInt64(8, true));
-    const pointerView = new Deno.UnsafePointerView(pointer);
+    const pointerView = new Deno.UnsafePointerView(pointer!);
 
     let list = [];
     for (let i = 0; i < length; i++) {
       const stringPointer = pointerView.getPointer(i * 8);
-      const stringView = new Deno.UnsafePointerView(stringPointer);
+      const stringView = new Deno.UnsafePointerView(stringPointer!);
       list.push(stringView.getCString());
     }
 

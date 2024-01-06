9edfab4a061844b5a5edf344e4c3a2c602e9e9a5
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-06 13:47:42 +0100

    feat: finish audio.ts

--- src/audio.ts
+++ src/audio.ts
@@ -1,6 +1,31 @@
-// TODO
-// RLAPI void InitAudioDevice(void);                                     // Initialize audio device and context
-// RLAPI void CloseAudioDevice(void);                                    // Close the audio device and context
-// RLAPI bool IsAudioDeviceReady(void);                                  // Check if audio device has been initialized successfully
-// RLAPI void SetMasterVolume(float volume);                             // Set master volume (listener)
-// RLAPI float GetMasterVolume(void);
+/**
+ * Audio device functions
+ * @module
+ */
+
+export class AudioDevice {
+  /** Initialize audio device and context */
+  static init() {
+    lib.symbols.InitAudioDevice();
+  }
+
+  /** Close the audio device and context */
+  static close() {
+    lib.symbols.CloseAudioDevice();
+  }
+
+  /** Check if audio device has been initialized successfully */
+  static isReady() {
+    return !!lib.symbols.IsAudioDeviceReady();
+  }
+
+  /** Set master volume (listener) */
+  static setMasterVolume(volume: number) {
+    lib.symbols.SetMasterVolume(volume);
+  }
+
+  /** Get master volume (listener) */
+  static getMasterVolume() {
+    return lib.symbols.GetMasterVolume();
+  }
+}

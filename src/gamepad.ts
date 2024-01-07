4c716fa5b647e1c8f77031988d27e75d629a0356
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-07 13:48:50 +0100

    feat: complete gamepad

--- src/gamepad.ts
+++ src/gamepad.ts
@@ -1,11 +1,61 @@
-// TODO
-// RLAPI bool IsGamepadAvailable(int gamepad);                   // Check if a gamepad is available
-// RLAPI const char *GetGamepadName(int gamepad);                // Get gamepad internal name id
-// RLAPI bool IsGamepadButtonPressed(int gamepad, int button);   // Check if a gamepad button has been pressed once
-// RLAPI bool IsGamepadButtonDown(int gamepad, int button);      // Check if a gamepad button is being pressed
-// RLAPI bool IsGamepadButtonReleased(int gamepad, int button);  // Check if a gamepad button has been released once
-// RLAPI bool IsGamepadButtonUp(int gamepad, int button);        // Check if a gamepad button is NOT being pressed
-// RLAPI int GetGamepadButtonPressed(void);                      // Get the last gamepad button pressed
-// RLAPI int GetGamepadAxisCount(int gamepad);                   // Get gamepad axis count for a gamepad
-// RLAPI float GetGamepadAxisMovement(int gamepad, int axis);    // Get axis movement value for a gamepad axis
-// RLAPI int SetGamepadMappings(const char *mappings);           // Set internal gamepad mappings (SDL_GameControllerDB)
+/**
+ * Gamepad utility functions
+ * @module
+ */
+import { lib } from "../bindings/bindings.ts";
+
+/** Gamepad functions */
+export class Gamepad {
+  /**  Check if a gamepad is available */
+  static isAvailable(gamepad: number) {
+    return !!lib.symbols.IsGamepadAvailable(gamepad);
+  }
+
+  /** Get gamepad internal name id */
+  static getName(gamepad: number) {
+    return new Deno.UnsafePointerView(lib.symbols.GetGamepadName(gamepad)!)
+      .getCString();
+  }
+
+  /** Check if a gamepad button has been pressed once */
+  static isButtonPressed(gamepad: number, button: number) {
+    return !!lib.symbols.IsGamepadButtonPressed(gamepad, button);
+  }
+
+  /** Check if a gamepad button is being pressed */
+  static isButtonDown(gamepad: number, button: number) {
+    return !!lib.symbols.IsGamepadButtonDown(gamepad, button);
+  }
+
+  /** Check if a gamepad button has been released once */
+  static isButtonReleased(gamepad: number, button: number) {
+    return !!lib.symbols.IsGamepadButtonReleased(gamepad, button);
+  }
+
+  /** Check if a gamepad button is being pressed */
+  static isButtonUp(gamepad: number, button: number) {
+    return !!lib.symbols.IsGamepadButtonUp(gamepad, button);
+  }
+
+  /** Get the last gamepad button pressed */
+  static getButtonPressed() {
+    return lib.symbols.GetGamepadButtonPressed();
+  }
+
+  /** Get gamepad axis count for a gamepad */
+  static getAxisCount(gamepad: number) {
+    return lib.symbols.GetGamepadAxisCount(gamepad);
+  }
+
+  /** Get gamepad axis count for a gamepad */
+  static getAxisMovement(gamepad: number, axis: number) {
+    return lib.symbols.GetGamepadAxisMovement(gamepad, axis);
+  }
+
+  /** Set internal gamepad mappings (SDL_GameControllerDB) */
+  static setMappings(mappings: string) {
+    return lib.symbols.SetGamepadMappings(
+      new TextEncoder().encode(mappings + "\0"),
+    );
+  }
+}
--- src/touch.ts
+++ src/touch.ts
@@ -8,27 +8,27 @@ import { Vector2 } from "./_util.ts";
 /** Touch functions */
 export class Touch {
   /** Get touch position X for touch point 0 (relative to screen size) */
-  getX() {
+  static getX() {
     return lib.symbols.GetTouchX();
   }
 
   /** Get touch position Y for touch point 0 (relative to screen size) */
-  getY() {
+  static getY() {
     return lib.symbols.GetTouchY();
   }
 
   /** Get touch position XY for a touch point index (relative to screen size) */
-  getPosition(index: number) {
+  static getPosition(index: number) {
     return Vector2.fromBuffer(lib.symbols.GetTouchPosition(index));
   }
 
   /** Get touch point identifier for given index */
-  getId(index: number) {
+  static getId(index: number) {
     return lib.symbols.GetTouchPointId(index);
   }
 
   /** Get number of touch points */
-  getCount() {
+  static getCount() {
     return lib.symbols.GetTouchPointCount();
   }
 }

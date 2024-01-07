0e1bfd9a67b78459429fbabd577c37ee222db677
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-07 13:01:18 +0100

    feat: add music bindings

--- src/music.ts
+++ src/music.ts
@@ -1,17 +1,98 @@
-// TODO
-// RLAPI Music LoadMusicStream(const char *fileName);                    // Load music stream from file
-// RLAPI Music LoadMusicStreamFromMemory(const char *fileType, const unsigned char *data, int dataSize); // Load music stream from data
-// RLAPI bool IsMusicReady(Music music);                                 // Checks if a music stream is ready
-// RLAPI void UnloadMusicStream(Music music);                            // Unload music stream
-// RLAPI void PlayMusicStream(Music music);                              // Start music playing
-// RLAPI bool IsMusicStreamPlaying(Music music);                         // Check if music is playing
-// RLAPI void UpdateMusicStream(Music music);                            // Updates buffers for music streaming
-// RLAPI void StopMusicStream(Music music);                              // Stop music playing
-// RLAPI void PauseMusicStream(Music music);                             // Pause music playing
-// RLAPI void ResumeMusicStream(Music music);                            // Resume playing paused music
-// RLAPI void SeekMusicStream(Music music, float position);              // Seek music to a position (in seconds)
-// RLAPI void SetMusicVolume(Music music, float volume);                 // Set volume for music (1.0 is max level)
-// RLAPI void SetMusicPitch(Music music, float pitch);                   // Set pitch for a music (1.0 is base level)
-// RLAPI void SetMusicPan(Music music, float pan);                       // Set pan for a music (0.5 is center)
-// RLAPI float GetMusicTimeLength(Music music);                          // Get music time length (in seconds)
-// RLAPI float GetMusicTimePlayed(Music music);                          // Get current music time played (in seconds)
+/**
+ * Music functions
+ * @module
+ */
+import { lib } from "../bindings/bindings.ts";
+
+/** A simple class for interacting with music */
+export class Music {
+  #buffer: ArrayBuffer;
+  /** Avoid using if at all possible */
+  constructor(buffer: ArrayBuffer) {
+    this.#buffer = buffer;
+  }
+
+  /** Load music stream from file */
+  static loadStream(filename: string) {
+    const encoded = new TextEncoder().encode(filename + "\0");
+    return new Music(lib.symbols.LoadMusicStream(encoded));
+  }
+
+  /** Load music stream from data */
+  static loadStreamFromMemory(fileType: string, data: Uint8Array) {
+    const encoded = new TextEncoder().encode(fileType + "\0");
+    return new Music(
+      lib.symbols.LoadMusicStreamFromMemory(encoded, data, data.length),
+    );
+  }
+
+  /** Checks if a music stream is ready */
+  isReady() {
+    return !!lib.symbols.IsMusicReady(this.#buffer);
+  }
+
+  /** Unload music stream */
+  unload() {
+    lib.symbols.UnloadMusicStream(this.#buffer);
+  }
+
+  /** Start music playing */
+  play() {
+    lib.symbols.PlayMusicStream(this.#buffer);
+  }
+
+  /** Check if music is playing */
+  isPlaying() {
+    return !!lib.symbols.IsMusicStreamPlaying(this.#buffer);
+  }
+
+  /** Updates buffers for music streaming */
+  update() {
+    lib.symbols.UpdateMusicStream(this.#buffer);
+  }
+
+  /** Stop music playing */
+  stop() {
+    lib.symbols.StopMusicStream(this.#buffer);
+  }
+
+  /** Pause music playing */
+  pause() {
+    lib.symbols.PauseMusicStream(this.#buffer);
+  }
+
+  /** Resume playing paused music */
+  resume() {
+    lib.symbols.ResumeMusicStream(this.#buffer);
+  }
+
+  /** Seek music to a position (in seconds) */
+  seek(position: number) {
+    lib.symbols.SeekMusicStream(this.#buffer, position);
+  }
+
+  /** Set volume for music (1.0 is max level) */
+  setVolume(volume: number) {
+    lib.symbols.SetMusicVolume(this.#buffer, volume);
+  }
+
+  /** Set pitch for a music (1.0 is base level) */
+  setPitch(pitch: number) {
+    lib.symbols.SetMusicPitch(this.#buffer, pitch);
+  }
+
+  /** Set pan for a music (0.5 is center) */
+  setPan(pan: number) {
+    lib.symbols.SetMusicPan(this.#buffer, pan);
+  }
+
+  /** Get music time length (in seconds) */
+  getTimeLength() {
+    return lib.symbols.GetMusicTimeLength(this.#buffer);
+  }
+
+  /** Get current music time played (in seconds) */
+  getTimePlayed() {
+    return lib.symbols.GetMusicTimePlayed(this.#buffer);
+  }
+}
--- src/touch.ts
+++ src/touch.ts
@@ -3,4 +3,4 @@
 // RLAPI int GetTouchY(void);                                    // Get touch position Y for touch point 0 (relative to screen size)
 // RLAPI Vector2 GetTouchPosition(int index);                    // Get touch position XY for a touch point index (relative to screen size)
 // RLAPI int GetTouchPointId(int index);                         // Get touch point identifier for given index
-// RLAPI int GetTouchPointCount(void);
+// RLAPI int GetTouchPointCount(void);                           // Get number of touch points

9a868394daee314d48894313a272f0f2d8ba044b
Lino Le Van <11367844+lino-levan@users.noreply.github.com>
2024-01-08 07:56:30 -0800

    feat: mostly finish shapes3d

--- src/shapes3d.ts
+++ src/shapes3d.ts
@@ -1,22 +1,251 @@
-// TODO
-// RLAPI void DrawLine3D(Vector3 startPos, Vector3 endPos, Color color);                                    // Draw a line in 3D world space
-// RLAPI void DrawPoint3D(Vector3 position, Color color);                                                   // Draw a point in 3D space, actually a small line
-// RLAPI void DrawCircle3D(Vector3 center, float radius, Vector3 rotationAxis, float rotationAngle, Color color); // Draw a circle in 3D world space
-// RLAPI void DrawTriangle3D(Vector3 v1, Vector3 v2, Vector3 v3, Color color);                              // Draw a color-filled triangle (vertex in counter-clockwise order!)
-// RLAPI void DrawTriangleStrip3D(Vector3 *points, int pointCount, Color color);                            // Draw a triangle strip defined by points
-// RLAPI void DrawCube(Vector3 position, float width, float height, float length, Color color);             // Draw cube
-// RLAPI void DrawCubeV(Vector3 position, Vector3 size, Color color);                                       // Draw cube (Vector version)
-// RLAPI void DrawCubeWires(Vector3 position, float width, float height, float length, Color color);        // Draw cube wires
-// RLAPI void DrawCubeWiresV(Vector3 position, Vector3 size, Color color);                                  // Draw cube wires (Vector version)
-// RLAPI void DrawSphere(Vector3 centerPos, float radius, Color color);                                     // Draw sphere
-// RLAPI void DrawSphereEx(Vector3 centerPos, float radius, int rings, int slices, Color color);            // Draw sphere with extended parameters
-// RLAPI void DrawSphereWires(Vector3 centerPos, float radius, int rings, int slices, Color color);         // Draw sphere wires
-// RLAPI void DrawCylinder(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color); // Draw a cylinder/cone
-// RLAPI void DrawCylinderEx(Vector3 startPos, Vector3 endPos, float startRadius, float endRadius, int sides, Color color); // Draw a cylinder with base at startPos and top at endPos
-// RLAPI void DrawCylinderWires(Vector3 position, float radiusTop, float radiusBottom, float height, int slices, Color color); // Draw a cylinder/cone wires
-// RLAPI void DrawCylinderWiresEx(Vector3 startPos, Vector3 endPos, float startRadius, float endRadius, int sides, Color color); // Draw a cylinder wires with base at startPos and top at endPos
-// RLAPI void DrawCapsule(Vector3 startPos, Vector3 endPos, float radius, int slices, int rings, Color color); // Draw a capsule with the center of its sphere caps at startPos and endPos
-// RLAPI void DrawCapsuleWires(Vector3 startPos, Vector3 endPos, float radius, int slices, int rings, Color color); // Draw capsule wireframe with the center of its sphere caps at startPos and endPos
-// RLAPI void DrawPlane(Vector3 centerPos, Vector2 size, Color color);                                      // Draw a plane XZ
-// RLAPI void DrawRay(Ray ray, Color color);                                                                // Draw a ray line
-// RLAPI void DrawGrid(int slices, float spacing);                                                          // Draw a grid (centered at (0, 0, 0))
+/**
+ * Functions for drawing 3d shapes
+ * @module
+ */
+import { lib } from "../bindings/bindings.ts";
+import { Color } from "./color.ts";
+import { Ray, Vector2, Vector3 } from "./_util.ts";
+
+/** Shape drawing functions */
+export class Shapes3D {
+  /** Draw a line in 3D world space */
+  static drawLine3D(startPos: Vector3, endPos: Vector3, color: Color) {
+    lib.symbols.DrawLine3D(startPos.buffer, endPos.buffer, color.buffer);
+  }
+
+  /** Draw a point in 3D space, actually a small line */
+  static drawPoint3D(position: Vector3, color: Color) {
+    lib.symbols.DrawPoint3D(position.buffer, color.buffer);
+  }
+
+  /** Draw a circle in 3D world space */
+  static drawCircle3D(
+    center: Vector3,
+    radius: number,
+    rotationAxis: Vector3,
+    rotationAngle: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCircle3D(
+      center.buffer,
+      radius,
+      rotationAxis.buffer,
+      rotationAngle,
+      color.buffer,
+    );
+  }
+
+  /** Draw a color-filled triangle (vertex in counter-clockwise order!) */
+  static drawTriangle3D(v1: Vector3, v2: Vector3, v3: Vector3, color: Color) {
+    lib.symbols.DrawTriangle3D(v1.buffer, v2.buffer, v3.buffer, color.buffer);
+  }
+
+  // TODO
+  // RLAPI void DrawTriangleStrip3D(Vector3 *points, int pointCount, Color color);                            // Draw a triangle strip defined by points
+
+  /** Draw cube */
+  static drawCube(
+    position: Vector3,
+    width: number,
+    height: number,
+    length: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCube(position.buffer, width, height, length, color.buffer);
+  }
+
+  /** Draw cube (Vector version) */
+  static drawCubeV(position: Vector3, size: Vector3, color: Color) {
+    lib.symbols.DrawCubeV(position.buffer, size.buffer, color.buffer);
+  }
+
+  /** Draw cube wires */
+  static drawCubeWires(
+    position: Vector3,
+    width: number,
+    height: number,
+    length: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCubeWires(
+      position.buffer,
+      width,
+      height,
+      length,
+      color.buffer,
+    );
+  }
+
+  /** Draw cube wires (Vector version) */
+  static drawCubeWiresV(position: Vector3, size: Vector3, color: Color) {
+    lib.symbols.DrawCubeWiresV(position.buffer, size.buffer, color.buffer);
+  }
+
+  /** Draw sphere */
+  static drawSphere(centerPos: Vector3, radius: number, color: Color) {
+    lib.symbols.DrawSphere(centerPos.buffer, radius, color.buffer);
+  }
+
+  /** Draw sphere with extended parameters */
+  static drawSphereEx(
+    centerPos: Vector3,
+    radius: number,
+    rings: number,
+    slices: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawSphereEx(
+      centerPos.buffer,
+      radius,
+      rings,
+      slices,
+      color.buffer,
+    );
+  }
+
+  /** Draw sphere wires */
+  static drawSphereWires(
+    centerPos: Vector3,
+    radius: number,
+    rings: number,
+    slices: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawSphereWires(
+      centerPos.buffer,
+      radius,
+      rings,
+      slices,
+      color.buffer,
+    );
+  }
+
+  /** Draw a cylinder/cone */
+  static drawCylinder(
+    position: Vector3,
+    radiusTop: number,
+    radiusBottom: number,
+    height: number,
+    slices: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCylinder(
+      position.buffer,
+      radiusTop,
+      radiusBottom,
+      height,
+      slices,
+      color.buffer,
+    );
+  }
+
+  /** Draw a cylinder with base at startPos and top at endPos */
+  static drawCylinderEx(
+    startPos: Vector3,
+    endPos: Vector3,
+    startRadius: number,
+    endRadius: number,
+    sides: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCylinderEx(
+      startPos.buffer,
+      endPos.buffer,
+      startRadius,
+      endRadius,
+      sides,
+      color.buffer,
+    );
+  }
+
+  /** Draw a cylinder/cone wires */
+  static drawCylinderWires(
+    position: Vector3,
+    radiusTop: number,
+    radiusBottom: number,
+    height: number,
+    slices: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCylinderWires(
+      position.buffer,
+      radiusTop,
+      radiusBottom,
+      height,
+      slices,
+      color.buffer,
+    );
+  }
+
+  /** Draw a cylinder wires with base at startPos and top at endPos */
+  static drawCylinderWiresEx(
+    startPos: Vector3,
+    endPos: Vector3,
+    startRadius: number,
+    endRadius: number,
+    sides: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCylinderWiresEx(
+      startPos.buffer,
+      endPos.buffer,
+      startRadius,
+      endRadius,
+      sides,
+      color.buffer,
+    );
+  }
+
+  /** Draw a capsule with the center of its sphere caps at startPos and endPos */
+  static drawCapsule(
+    startPos: Vector3,
+    endPos: Vector3,
+    radius: number,
+    slices: number,
+    rings: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCapsule(
+      startPos.buffer,
+      endPos.buffer,
+      radius,
+      slices,
+      rings,
+      color.buffer,
+    );
+  }
+
+  /** Draw capsule wireframe with the center of its sphere caps at startPos and endPos */
+  static drawCapsuleWires(
+    startPos: Vector3,
+    endPos: Vector3,
+    radius: number,
+    slices: number,
+    rings: number,
+    color: Color,
+  ) {
+    lib.symbols.DrawCapsuleWires(
+      startPos.buffer,
+      endPos.buffer,
+      radius,
+      slices,
+      rings,
+      color.buffer,
+    );
+  }
+
+  /** Draw a plane XZ */
+  static drawPlane(centerPos: Vector3, size: Vector2, color: Color) {
+    lib.symbols.DrawPlane(centerPos.buffer, size.buffer, color.buffer);
+  }
+
+  /** Draw a ray line */
+  static drawRay(ray: Ray, color: Color) {
+    lib.symbols.DrawRay(ray.buffer, color.buffer);
+  }
+
+  /** Draw a grid (centered at (0, 0, 0)) */
+  static drawGrid(slices: number, spacing: number) {
+    lib.symbols.DrawGrid(slices, spacing);
+  }
+}
